31695d91a18422816127977b5d82eee791f0d860
bms_007 <168493571+Bernardinomarques007@users.noreply.github.com>
2025-06-14 19:08:55 -0300

    Update num.py

--- num.py
+++ num.py
@@ -1,16 +1,21 @@
+# Desafio: Classificador de Números em Intervalos
+# Crie uma função chamada classificar_numero que recebe um número e retorna uma
+# mensagem com base no intervalo em que ele se encontra:
+
+
 def classificar_numero(num):
 
     if num < 0 :
-        print("Número negativo")
+        print("| Número negativo")
     elif num > 0 and num <=10:
-        print("Entre 0 e 10")
+        print("| Entre 0 e 10")
     elif num > 10 and num <=100:
-        print("Entre 10 e 100")
+        print("| Entre 10 e 100")
     elif num > 100:
-        print("Maior que 100")
+        print("| Maior que 100")
     else:
-        print("Você não digitou um número, ou seu numero é igual a 0")
+        print("| Você não digitou um número, ou seu numero é igual a 0")
 
 
-print(classificar_numero(float(input("Digite seu numero " ))))
+print(classificar_numero(float(input("| Digite seu numero " ))))
 
